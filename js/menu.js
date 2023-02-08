cefe0f9ab3d26b7654aa796c05b6c5d3bfd788fb
joramot <mojica.raul@gmail.com>
2023-02-08 16:44:38 -0600

    Fix issues of the dropdown menu

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,18 @@
-// Hamburger menu
-const btn = document.querySelector('#hamburger');
-const nav = document.querySelector('.nav nav');
-const openNav = document.querySelector('.menu-display');
-const closeBtn = document.querySelector('.xicon');
-const menuOpt = document.querySelectorAll('#items-menus li a');
+// Dropdown Menu
+const btncls = document.querySelector('#closeOnClick');
+const btnopn = document.querySelector('#openOnClick');
+const menuOpt = document.querySelector('.overlay-content a')
 
-const toggleMenuDisplay = () => {
-  if (nav.style.display !== 'none') {
-    nav.style.display = 'none';
-    openNav.style.display = 'flex';
-  } else {
-    nav.style.display = 'grid';
-    openNav.style.display = 'none';
-  }
-};
+const btnOpnNav = () => {
+  document.getElementById("myNav").style.height = "100%";
+}
 
-btn.addEventListener('click', toggleMenuDisplay);
-closeBtn.addEventListener('click', toggleMenuDisplay);
+const btncloseNav = () => {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+btnopn.addEventListener('click', btnOpnNav);
+btncls.addEventListener('click', btncloseNav);
 menuOpt.forEach((link) => {
-  link.addEventListener('click', toggleMenuDisplay);
+  link.addEventListener('click', btncloseNav);
 });
