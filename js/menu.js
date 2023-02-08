75c24d6e30c276f8de093421ad4c252b5aea0f11
joramot <mojica.raul@gmail.com>
2023-02-08 08:41:38 -0600

    Fixed class styles.

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 // Hamburger menu
 const btn = document.querySelector('#hamburger');
-const nav = document.querySelector('.topnav .header-nav');
+const nav = document.querySelector('.nav nav');
 const openNav = document.querySelector('.menu-display');
 const closeBtn = document.querySelector('.xicon');
 const menuOpt = document.querySelectorAll('#items-menus li a');
